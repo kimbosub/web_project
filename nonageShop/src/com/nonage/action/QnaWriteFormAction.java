0747f199b812bd6a2bf4659892e2f87fa187db2d
kbs <big2-01@clo-15-PC>
2018-11-14 11:00:33 +0900

    add : sibalram

--- nonageShop/src/com/nonage/action/QnaWriteFormAction.java
+++ nonageShop/src/com/nonage/action/QnaWriteFormAction.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.nonage.dto.MemberVO;
 
 public class QnaWriteFormAction implements Action {
 
